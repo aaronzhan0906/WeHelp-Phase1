806a64a7833f63faa20ed25effd9de4d29591aa1
ChengTing <aaronzhan0906@gmail.com>
2024-05-07 17:18:36 +0800

    20240507

--- week6/main.py
+++ week6/main.py
@@ -1,17 +1,16 @@
-from fastapi import FastAPI, Request, Form, status, Path
+from fastapi import FastAPI, Request, Form, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 import database
-mycusor = database.mydb.cursor()
-
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
+mycursor = database.mydb.cursor()
 
 
 # Home
@@ -27,43 +26,26 @@ async def home(request: Request):
 # Signin
 @app.post("/signin")
 async def signin(request: Request):
+    # Get form data
     form_data = await request.form()
-    username = form_data.get("username", "")
-    password = form_data.get("password", "")
-    
-    if username == "test" and password == "test":
+    username = form_data.get("signin-username", "")
+    password = form_data.get("signin-password", "")
+
+    # Check if user exists by username and password
+    mycursor.execute("SELECT id, user, username , password FROM member WHERE username = %s AND password = %s", (username, password))
+    member = mycursor.fetchone()  
+
+    if member:
         session = request.session
         session["SIGNED-IN"] = True
-        session["user"] = "user" # 等下要從資料庫拿
+        session["id"] = {"user": member[0]}
+        session["user"] = {"user": member[1]}
+        session["username"] = {"user": member[2]}
         return RedirectResponse(url="/member", status_code=status.HTTP_302_FOUND)
-    elif not username or not password:
-        error_message = "請輸入帳號、密碼"
-        return RedirectResponse(url=f"/error?message={error_message}", status_code=status.HTTP_302_FOUND)
     else:
-        error_message = "帳號、密碼輸入錯誤"
+        error_message = "Username or password is not correct"
         return RedirectResponse(url=f"/error?message={error_message}", status_code=status.HTTP_302_FOUND)
-
-
-# Signup
-@app.post("/signup", response_class=HTMLResponse)
-async def signup(request: Request):
-    form_data = await request.form()
-    user = form_data.get("signup-user", "")
-    username = form_data.get("signup-username", "")
-    password = form_data.get("signup-password", "")
-    print (user, username, password)
-
-    # check if user exists
-    mycusor.execute("SELECT * FROM member WHERE username = %s", (username,))
-    check_result = mycusor.fetchall()
-    if check_result:
-        error_message = "Repeated username"
-        return RedirectResponse(url=f"/error?message={error_message}", status_code=status.HTTP_302_FOUND)
-    else:
-        #補完
-
-    return RedirectResponse(url="/member", status_code=status.HTTP_302_FOUND)
-
+    
 
 # Success Page
 @app.get("/member", response_class=HTMLResponse)
@@ -71,7 +53,7 @@ async def success_page(request: Request):
     session = request.session
     if session.get("SIGNED-IN", False):
         user = session.get("user", "") 
-        return templates.TemplateResponse("member.html", {"request": request, "user": user})
+        return templates.TemplateResponse("member.html", {"request": request, "user": user["user"]})
     else:
         return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
 
@@ -88,6 +70,35 @@ async def error_page(request: Request, message: str = None):
 async def signout(request: Request):
     session = request.session
     session.pop("SIGNED-IN", False)  
+    session.pop("id", None)
+    session.pop("user", None)
+    session.pop("username", None)
+    print(request.session)
     return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+
+
+
+# Signup
+@app.post("/signup", response_class=HTMLResponse)
+async def signup(request: Request):
+    form_data = await request.form()
+    user = form_data.get("signup-user", "")
+    username = form_data.get("signup-username", "")
+    password = form_data.get("signup-password", "")
+    print (user, username, password)
+
+    # check if user exists
+    mycursor.execute("SELECT * FROM member WHERE username = %s", (username,))
+    check_result = mycursor.fetchone()
+
+    if check_result:
+        error_message = "Repeated username"
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=status.HTTP_302_FOUND)
+    else:
+        mycursor.execute("INSERT INTO member (user, username, password) VALUES (%s, %s, %s)", (user, username, password))
+        database.mydb.commit()
+        return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+
+
 
 
--- week6/static/main.js
+++ week6/static/main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("login-form");
-    const agreeCheckbox = document.getElementById("agree-check-box");
+    const signinForm = document.getElementById("signin-form");
     const signupForm = document.getElementById("signup-form");
-
+    
+  
     signupForm.addEventListener("submit", function(event) {
-        const inputs = signupForm.querySelectorAll("input[name='signup-user'], input[name='signup-password'], input[name='signup-password-confirm']");
+        const inputs = signupForm.querySelectorAll("input[name='signup-user'], input[name='signup-username'], input[name='signup-password']");
         let isEmpty = false;
         inputs.forEach(function(input) {
             if (input.value.trim() === "") {
-                isEmpty = true;
+            isEmpty = true;
             }
         });
         if (isEmpty) {
@@ -16,11 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
         }
     });
-
-    loginForm.addEventListener("submit", function(event) {
-        if (!agreeCheckbox.checked) {
-            alert("Please check the checkbox first.");
-            event.preventDefault(); // Prevent form submission
+  
+    signinForm.addEventListener("submit", function(event) {
+        const inputs = signinForm.querySelectorAll("input[name='signin-username'], input[name='signin-password']");
+        let isEmpty = false;
+        inputs.forEach(function(input) {
+          if (input.value.trim() === "") {
+            isEmpty = true;
+          }
+        });
+        if (isEmpty) {
+          alert("Please fill in all fields.");
+          event.preventDefault();
         }
     });
-});
+  });
